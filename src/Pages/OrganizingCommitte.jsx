2cc5c4a3c655a9baf962a046f96186f8ba54ef36
ramkrishna mondal <ramkrishnam170@gmail.com>
2026-02-01 22:23:15 +0530

    feat: Add new page to display the conference organizing committee members.

--- src/Pages/OrganizingCommitte.jsx
+++ src/Pages/OrganizingCommitte.jsx
@@ -92,7 +92,7 @@ const OrganizingCommittee = () => {
               name: "Prof. Rohit Mehra",
               designation: "Dean Research & Consultancy",
               institute: "Dr. B. R. Ambedkar National Institute of Technology, Jalandhar",
-              department: "Physics",
+              department: "",
               photo: rohitMehraPhoto,
             },
             {
